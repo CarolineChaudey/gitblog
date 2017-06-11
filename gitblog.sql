1d55ba4552d37ad1ff67e47547f0761f8ce0a3f1
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-11 12:35:31 +0200

    ajout script sql corrige

---
+++ gitblog.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `Comment` (
+  `id` int(10) NOT NULL,
+  `content` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `created_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `author_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `Post` (
+  `id` int(10) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `contenu` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` datetime NOT NULL,
+  `autor_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `User` (
+  `id` int(10) NOT NULL,
+  `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `role` varchar(45) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `Comment`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `Post`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `Comment`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Post`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `User`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
