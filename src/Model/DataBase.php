582e4aa50a838391f8d292ef991c0f836493756c
Coignard Hervé <herve.coignard@pims.io>
2017-06-29 23:37:05 +0200

    Changement fichier html en phtml Correction de la connection a la bdd

--- src/Model/DataBase.php
+++ src/Model/DataBase.php
@@ -10,11 +10,21 @@ class DataBase {
 	
 	// Database object
 	private $dataBase;
-	
+
+	private $servername = "localhost";
+	private $username 	= "root";
+	private $password 	= "xxx";
+	private $dbname		= "gitblog";
+
+
 	function __construct () {
-		$this->dataBase = new PDO('mysql:......;charset=utf8', 'username', 'password');
-		$this->dataBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$this->dataBase->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		try {
+			$this->dataBase = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+			$this->dataBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->dataBase->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		} catch(PDOException $e) {
+			// TODO return error page
+		}
 	}
 	
 }
--- src/routes.php
+++ src/routes.php
@@ -4,27 +4,15 @@
 	require 'FormVerif.php';
 	require 'Security.php';
 
-	$servername = "localhost";
-	$username 	= "root";
-	$password 	= "xxx";
-	$dbname		= "gitblog";
-
-	try {
-		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-		// set the PDO error mode to exception
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} catch(PDOException $e) {
-		// TODO return error page
-	}
 
 	//accueil
 	$app->get('/', function(Request $request, Response $response) {
-		return $this->view->render($response, 'index.html');
+		return $this->view->render($response, 'index.phtml');
 	});
 
 	//inscription
 	$app->get('/join', function(Request $request, Response $response) {
-		return $this->view->render($response, 'join.html');
+		return $this->view->render($response, 'join.phtml');
 	});
 
 	//inscription
@@ -35,7 +23,10 @@
 		}
 		$security = new Security;
 		$_POST["pswd"] = $security->cryptPassword($_POST["pswd"]);
-		/*
+		
+		// Récuperation de la connection
+		$conn = new DataBase();
+		
 		$query = "insert into User('username', 'email', 'password', 'role') "
 							. "values("
 							. $_POST["login"] . ", "
@@ -47,8 +38,8 @@
 		if ($result !== 1) {
 			return "error : l'inscription a échoué.";
 		}
-		*/
-		return $this->view->render($response, 'index.html');
+		
+		return $this->view->render($response, 'index.phtml');
 	});
 
 ?>
