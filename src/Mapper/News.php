d47fb7dd6028af1981e189e41c1a0f4e3751bd43
herve <hobito@hotmail.fr>
2017-07-23 22:29:04 +0200

    #3 read et update des articles

--- src/Mapper/News.php
+++ src/Mapper/News.php
@@ -14,4 +14,16 @@ class News {
 		$prepare->execute();
 		return $prepare->fetchAll();
 	}
+	public function getNew(int $id) {
+		$prepare = $this->database->prepare("SELECT * FROM Post WHERE id = $id");
+		$prepare->execute();
+		return $prepare->fetch();
+	}
+	public function updateNew(int $id, String $title, String $content) {
+		$prepare = $this->database->prepare("UPDATE Post SET title=:title, contenu=:content WHERE id = $id");
+		$prepare->execute([
+				'title' => $title,
+				'content' => $content
+		]);
+	}
 }
--- src/routes.php
+++ src/routes.php
@@ -12,12 +12,31 @@
 		$this->view->news = (new News())->getAllNews();
 		return $this->view->render($response, 'index.phtml');
 	});
-
+	
 	//inscription
 	$app->get('/join', function(Request $request, Response $response) {
 		return $this->view->render($response, 'join.phtml');
 	});
-
+	
+	//affichage d'une new
+	$app->get('/new', function(Request $request, Response $response) {
+		$this->view->new = (new News())->getNew($_GET['id']);
+		return $this->view->render($response, 'new.phtml');
+	});
+	
+	//affichage d'une new
+	$app->get('/modify', function(Request $request, Response $response) {
+		$this->view->new = (new News())->getNew($_GET['id']);
+		return $this->view->render($response, 'modify.phtml');
+	});
+	
+	//affichage d'une new
+	$app->post('/applymodify', function(Request $request, Response $response) use ($app) {
+		(new News())->updateNew($_GET['id'],$_POST['title'], $_POST['content']);
+		return $response->withStatus(302)->withHeader('Location', '/');
+		//return $this->view->render($response, 'index.phtml');
+	});
+	
 	//connexion
 	$app->get('/login', function(Request $request, Response $response) {
 		return $this->view->render($response, 'login.phtml');
